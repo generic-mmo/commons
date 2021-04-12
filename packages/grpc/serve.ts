b7b553b40aad65dc0c1c7b593eccfc3db917f63f
Mateusz Pietrzak <57403837+mzpkjs@users.noreply.github.com>
2021-04-12 14:41:54 +0200

    chore: serve returns a promise

--- packages/grpc/serve.ts
+++ packages/grpc/serve.ts
@@ -3,11 +3,18 @@ import { MicroserviceConstructor } from "./main";
 
 
 
-export default function serve(microservice: unknown) {
+export default function serve(microservice: unknown): Promise<Server> {
     const { implementation, configuration } = microservice as MicroserviceConstructor
     const server = new Server()
-    server.addService(configuration.service, new implementation() as Parameters<typeof server.addService>[1])
-    server.bindAsync(configuration.location(), ServerCredentials.createInsecure(), () => {
-        server.start()
+    return new Promise((resolve, reject) => {
+        server.addService(configuration.service, new implementation() as Parameters<typeof server.addService>[1])
+        server.bindAsync(configuration.location(), ServerCredentials.createInsecure(), (error) => {
+            if (error) {
+                reject(error)
+            } else {
+                server.start()
+                resolve(server)
+            }
+        })
     })
 }
